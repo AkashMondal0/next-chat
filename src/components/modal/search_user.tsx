ead97241e4e96ef248e3cccbd2d32b8b417ec5e0
Akash Mondal <akash2003mondal@gmail.com>
2023-11-01 19:37:34 +0530

    already in conversation, change prisma models

--- src/components/modal/search_user.tsx
+++ src/components/modal/search_user.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Modal } from '../shared/Modal';
 import SearchCommand from '../shared/SearchCommand';
 import axios from 'axios';
@@ -9,23 +9,30 @@ import useClientProfile from '@/hooks/client-profile';
 import { Users } from 'lucide-react';
 import { Button } from '../ui/button';
 
+const fetchUsers = async () => {
+  const res = await axios.get('/api/users');
+  return res.data;
+}
 interface SearchModalProps { }
+
 const SearchModal: FC<SearchModalProps> = () => {
   const currentProfile = useClientProfile()
 
-  const fetchUsers = async () => {
-    const res = await axios.get('/api/users');
-    return res.data;
+  const filterUser = (users: User[]) => {
+    const removeConversationUsers = users.filter(user => !currentProfile.conversations.map((c) => c.users.includes(user)));
+    return removeConversationUsers.filter(user => user.id !== currentProfile?.state.id)
   }
 
   const { status, data, error } = useQuery<User[]>({
-    queryKey: ['users'],
+    queryKey: ['usersList'],
     queryFn: fetchUsers,
   })
 
-  return (<Modal title={"Search User"} trigger={<Button variant={"ghost"}><Users className='w-6 h-6 cursor-pointer' /></Button>}>
+  return (<Modal title={"Search User"} trigger={<Button variant={"ghost"}>
+    <Users className='w-6 h-6 cursor-pointer' />
+  </Button>}>
     <SearchCommand
-      data={data?.filter(user => user.id !== currentProfile.state.id)}
+      data={filterUser(data ? data : [])}
       status={status}
       error={error?.message} />
   </Modal>)
